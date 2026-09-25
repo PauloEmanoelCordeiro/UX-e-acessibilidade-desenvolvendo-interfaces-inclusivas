b88a1bc5ad5b7bd3dea5f33a4e174e1a924745d2
PauloEmanoelCordeiro <paulo.emanoel.cordeiro@escola.pr.gov.br>
2026-09-25 09:30:06 -0300

    Alterações

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnAjuda = document.queryselector(".botao-ajuda");
+let btnFechar = document.queryselector(".botao-fechar");
+let modal = document.queryselector(".modal-fundo");
+
+btnAjuda.addEventListener("click", abreModal);
+btnFechar.addEventListener("click", fecharModal);
